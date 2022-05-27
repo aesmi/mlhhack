509b512ed8f36ff6aeedb0d2c6d6846ca4cdb6f8
snekolen <helen.ma121601@gmail.com>
2022-05-26 21:23:00 -0500

    Reformatted commands

--- MLH.py
+++ MLH.py
@@ -1,6 +1,8 @@
 
 import discord
-import random
+import random as r
+from discord.ext import commands
+
 # make a function that pulls the token, because this github is public
 def read_token() -> str:
     with open("token.txt", "r") as f:
@@ -12,17 +14,63 @@ TOKEN = read_token()
 
 
 # initialize the client
-client = discord.Client()
+client = commands.Bot(command_prefix = "!")
 
 
 # outputs a message in local terminal confirming connection
 @client.event
 async def on_ready():
-    print('we have logged in as {0.user}'.format(client))
+    #print('we have logged in as {0.user}'.format(client))
+    print('yeet')
 
 bot_output = "botspam"
 
+#99 command
+@client.command(name = "99")
+async def quotes99(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'I\'m the human form of the 💯 emoji.'
+    ]
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+    return
 
+#sus command
+@client.command(name = "sus")
+async def sus(ctx):
+    await ctx.send("ඞ")
+    return
+
+#hello command
+@client.command(name = "hello")
+async def hello(ctx):
+    await ctx.send(f'Hello, ' + ctx.message.author.display_name + '!')
+    return
+
+#bye command
+@client.command(name = "bye")
+async def bye(ctx):
+    await ctx.send(f'See you later, ' + ctx.message.author.display_name + '!')
+    return
+
+
+#Random command
+@client.command(name = "random")
+async def random(ctx):
+    randNum = r.randrange(0, 1000)
+    res = 'This is your random number: ' + str(randNum)
+    await ctx.send(res)
+    return
+
+#Anywhere command
+@client.command(name = "anywhere")
+async def random(ctx):
+    await ctx.send('this can be used anywhere')
+    return
+
+
+'''
 @client.event
 async def on_message(message):
     # simplifies input
@@ -74,7 +122,7 @@ async def on_message(message):
         await message.channel.send('this can be used anywhere')
         return
 
-
+'''
 
 
 # run our bot by providing the token
