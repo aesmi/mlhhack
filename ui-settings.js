741eb449245f6a274741a4453ac63074b735c741
snekolen <54336823+snekolen@users.noreply.github.com>
2022-05-27 18:00:42 -0500

    Added framework to frontend

---
+++ ui-settings.js
@@ -0,0 +1,17 @@
+function getNumber() {
+    var input = document.getElementById("num-input"). value;
+    var topMessage = document.getElementById("error");
+    var c = document.getElementById("code").value;
+    var n = "+" + c.toString() + input;
+
+    if(input.length != 10 || isNaN(input)) { //Invalid input
+        topMessage.innerText = "Please enter a valid phone number";
+        return ;
+    }
+    
+    var page1 = document.getElementById("page1");
+    var page2 = document.getElementById("page2");
+
+    page1.style.display = "none";
+    page2.style.display = "block";
+}
