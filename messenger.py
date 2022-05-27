cd6e8300b65846bdeb2ad19f7d9349ad8f133155
snekolen <54336823+snekolen@users.noreply.github.com>
2022-05-27 12:40:15 -0500

    Added content to functions

--- messenger.py
+++ messenger.py
@@ -5,20 +5,23 @@ TOKEN = ""
 
 client = Client(SID, TOKEN)
 
-message = client.messages.create(
-    to = "", #Write number here
-    from_ = "", #Insert Twilio number
-    body = "Hello World!"
-)
-
-print(message.sid)
+#print(message.sid)
 
 #Function to notify user their luggage is on the conveyer belt
-def cNotif(phoneNo, lName): #Phone number, luggage name/identifier
-    #if lname status is equal to conveyor belt value, then send the notification, otherwise do nothing
-    pass
+def cNotif(phoneNo, luggageID): #Phone number, luggageID
+    content = "Your luggage with ID " + luggageID + " is on the conveyor belt and ready for pickup"
+    message = client.messages.create(
+        to = phoneNo, #Write number here
+        from_ = "", #Insert Twilio number
+        body = content
+    )
+    return
 
-#Function to confirm to users their luggage has been picked up
-def pNotif(phoneNo, lName): #Phone number, luggage name/identifier
-    #if lname status is picked up, then send notification, otherwise do nothing
-    pass
+#Function to confirm to users all their luggage has been picked up
+def pNotif(phoneNo): #Phone number, luggage name/identifier
+    message = client.messages.create(
+        to = phoneNo, #Write number here
+        from_ = "", #Insert Twilio number
+        body = "Your luggage has been picked up"
+    )
+    return
