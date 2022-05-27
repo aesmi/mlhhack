5f44f2ffe03995042bc4156afca4817d2bbdbd65
noahkilpatrick <62220418+noahkilpatrick@users.noreply.github.com>
2022-05-27 12:36:37 -0500

    comment

--- messenger.py
+++ messenger.py
@@ -15,8 +15,10 @@ print(message.sid)
 
 #Function to notify user their luggage is on the conveyer belt
 def cNotif(phoneNo, lName): #Phone number, luggage name/identifier
+    #if lname status is equal to conveyor belt value, then send the notification, otherwise do nothing
     pass
 
 #Function to confirm to users their luggage has been picked up
 def pNotif(phoneNo, lName): #Phone number, luggage name/identifier
+    #if lname status is picked up, then send notification, otherwise do nothing
     pass
