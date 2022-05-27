7d7b68291fd21bbd8c94024d6368cde2542d35cf
snekolen <54336823+snekolen@users.noreply.github.com>
2022-05-27 13:47:56 -0500

    Updated messages

--- messenger.py
+++ messenger.py
@@ -1,27 +1,35 @@
 from twilio.rest import Client
+import datetime
+import json
 
-SID = ""
-TOKEN = ""
+file = open("twilio-info.json")
+data = json.load(file)
+
+SID = data["SID"]
+TOKEN = data["TOKEN"]
+twilioNum = data["Phone-no"]
 
 client = Client(SID, TOKEN)
 
-#print(message.sid)
-
 #Function to notify user their luggage is on the conveyer belt
-def cNotif(phoneNo, luggageID): #Phone number, luggageID
-    content = "Your luggage with ID " + luggageID + " is on the conveyor belt and ready for pickup"
+def cNotif(phoneNo, luggageID, statNo): #Phone number, luggage ID, Station number
+    content = "Your luggage with ID " + luggageID + " is ready for pickup at " + statNo
     message = client.messages.create(
         to = phoneNo, #Write number here
-        from_ = "", #Insert Twilio number
+        from_ = twilioNum, #Insert Twilio number
         body = content
     )
     return
 
+
 #Function to confirm to users all their luggage has been picked up
-def pNotif(phoneNo): #Phone number, luggage name/identifier
+def pNotif(phoneNo, statNo): #Phone number, Station number
+    now = datetime.datetime.now()
+    tInfo = now.strftime("%b-%d-%Y %H:%M:%S")   
+
     message = client.messages.create(
         to = phoneNo, #Write number here
-        from_ = "", #Insert Twilio number
-        body = "Your luggage has been picked up"
+        from_ = twilioNum, #Insert Twilio number
+        body = "Your luggage has been picked up from " + statNo + " at " + tInfo
     )
     return
